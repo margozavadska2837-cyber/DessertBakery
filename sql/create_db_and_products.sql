1b2e688eba242c526725e8b0af3e74f1bcfe727a
margozavadska2837-cyber <margozavadska2837@gmail.com>
2025-12-05 05:50:20 +0200

    update project code

--- sql/create_db_and_products.sql
+++ sql/create_db_and_products.sql
@@ -1,7 +1,3 @@
--- create_db_and_products.sql
--- Виконай як адміністратор або користувач, що має права CREATE DATABASE
-
--- 1) Створити базу (якщо ще немає)
 IF DB_ID('FoodCatalog') IS NULL
 BEGIN
   CREATE DATABASE FoodCatalog;
@@ -11,18 +7,17 @@ GO
 USE FoodCatalog;
 GO
 
--- 2) Створити таблицю Products
 IF OBJECT_ID('dbo.Products', 'U') IS NULL
 BEGIN
-  CREATE TABLE dbo.Products (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    Name NVARCHAR(200) NOT NULL,
-    Description NVARCHAR(1000) NULL,
-    Price DECIMAL(10,2) NOT NULL,
-    Currency NVARCHAR(10) NOT NULL DEFAULT 'UAH',
-    CreatedAt DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(),
-    UpdatedAt DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME()
-  );
+  CREATE TABLE Products (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  name NVARCHAR(100),
+  description NVARCHAR(255),
+  price DECIMAL(18,2),
+  currency NVARCHAR(10),
+  createdAt DATETIME DEFAULT GETDATE(),
+  updatedAt DATETIME DEFAULT GETDATE()
+);
 END
 GO
 
